146f9e91f7a3d381c4c25cf4b4db961996334339
FarhanQuadri25 <zealfarhan@gmail.com>
2024-12-30 03:58:38 +0530

    second-commit

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -3,24 +3,23 @@ import Nav from "@/components/nav/Nav";
 import { DATA_LIST } from "@/data/data";
 import Image from "next/image";
 
-interface Params {
-  id: string;
-}
-
-interface Project {
-  id: number;
-  title: string;
-  description: string;
-  allImages?: {
-    first: string;
-    second: string;
-    third: string;
-    fourth: string;
-  };
-}
-
-export default async function SingleProject({ params }: { params: Params }) {
-  const { id } = params;
+export default async function SingleProject({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  interface Project {
+    id: number;
+    title: string;
+    description: string;
+    allImages?: {
+      first: string;
+      second: string;
+      third: string;
+      fourth: string;
+    };
+  }
 
   const project: Project | undefined = DATA_LIST.find(
     (project) => project.id === Number(id)
@@ -37,7 +36,7 @@ export default async function SingleProject({ params }: { params: Params }) {
         <div className="absolute inset-0 flex flex-col justify-between items-center text-white">
           <Nav />
           <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-28 capitalize text-center px-4">
-            {project.title}
+            {project?.title || "Project not found"}
           </h1>
         </div>
       </div>
@@ -53,7 +52,7 @@ export default async function SingleProject({ params }: { params: Params }) {
           />
         </h1>
         <p className="text-[#cecece] font-bold font-poppins text-sm sm:text-base md:text-lg lg:text-2xl mt-4 bg-slate-800 p-4 sm:p-6">
-          {project.description}
+          {project?.description || "No description available"}
         </p>
         <h1 className="text-4xl text-[#cecece] font-poppins font-bold uppercase flex items-center mt-8">
           Gallery
@@ -66,7 +65,7 @@ export default async function SingleProject({ params }: { params: Params }) {
           />
         </h1>
         <div className="flex flex-wrap mt-8 p-2 gap-4">
-          {project.allImages && (
+          {project?.allImages && (
             <>
               <Image
                 width={300}
